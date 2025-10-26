8414343a3ceb668b45dda79cc19afb2245af8705
PonyVicE <vladbeliy77@gmail.com>
2025-10-26 19:52:26 +0700

    header,content,footer

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
-  base: '/test21t/',
+  base: '/frontend-project-V2/',
   build: {
     emptyOutDir: true,
     modulePreload: { polyfill: false },
